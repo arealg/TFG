efeb85d7d15d40b1a82684501fd99b95b1428605
arealg <anavm4_@hotmail.com>
2017-08-14 19:44:48 +0200

    Broadcast bug

--- interpreter/js/app.js
+++ interpreter/js/app.js
@@ -71,7 +71,7 @@ function connectSession(token){
 initSession();
 connectSession(data.token);
 
-var OpenTok = require('opentok');
+var OpenTok = requirejs('opentok');
     
 var opentok = new OpenTok(data.apiKey, data.secret);
 
